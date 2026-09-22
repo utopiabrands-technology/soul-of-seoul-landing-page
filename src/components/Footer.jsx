a1ad4dd2501c9092fb138b093ad54dac72deee0b
utopiabrands-technology <technology@utopiabrands.com>
2026-09-22 23:23:39 +0500

    updated social links

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -12,7 +12,7 @@ export default function Footer() {
       <div className="footer-links">
         <div><a href="#our-story">Our Story</a><a href="#philosophy">Our Philosophy</a><a href="#masks">Hydrogel Masks</a><a href="#toner-pads">Toner Pads</a></div>
         <div className="footer-legal-links"><a href="#">Contact Us</a><a href="#">Privacy Policy</a><a href="#">Terms of Use</a></div>
-        <div className="footer-social-links"><a href="#">Instagram</a><a href="#">TikTok</a></div>
+        <div className="footer-social-links"><a href="https://www.instagram.com/soulofseoulbeauty/" target="_blank" rel="noreferrer">Instagram</a><a href="https://www.facebook.com/soulofseoulbeauty/" target="_blank" rel="noreferrer">Facebook</a></div>
       </div>
 
       <div className="footer-bottom"><span>© 2026 Soul of Seoul. All rights reserved.</span><a href="#top">Back to top ↑</a></div>
